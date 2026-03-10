fd02df19122699068e7cca47779de702ca794f8d
alexfiol269837-spec <alex.fiol269837@potros.itson.edu.mx>
2026-03-10 00:28:58 -0700

    Primera versionOrdenamientoBurbuja.java

--- OrdenamientoBurbuja.java
+++ OrdenamientoBurbuja.java
@@ -6,8 +6,6 @@ package com.mycompany.ordenamientoburbuja;
 
 
 /**
- * Implementación del algoritmo de ordenamiento burbuja
- * que ordena palabras según la longitud del texto.
  * 
  * @author fiolr
  */
@@ -17,60 +15,36 @@ public class OrdenamientoBurbuja {
 
         String[] palabras = {"uno", "union", "ulala", "u", "ucraniano", "umamusume"};
 
-        System.out.println("Antes de ordenarlo estaba asi:"); // 1 operación
+        System.out.println("Antes de ordenarlo estaba asi:")
 
-        // recorrer arreglo para imprimir
-        for (String palabra : palabras) { // n operaciones
+  
+        for (String palabra : palabras) {
             System.out.print(palabra + " ");
         }
 
-        // Algoritmo de ordenamiento burbuja
+   
         for (int i = 0; i < palabras.length - 1; i++) { 
-            // inicialización = 1
-            // comparación = n
-            // incremento = n
+      
 
             for (int j = 0; j < palabras.length - i - 1; j++) {
-                // inicialización = 1
-                // comparación = n
-                // incremento = n
+    
+                if (palabras[j].length() > palabras[j + 1].length()) { 
 
-                // comparación de longitud de palabras
-                if (palabras[j].length() > palabras[j + 1].length()) { // 1 comparación
-
-                    // intercambio de posiciones
-                    String temp = palabras[j];     // 1 asignación
-                    palabras[j] = palabras[j + 1]; // 1 asignación
-                    palabras[j + 1] = temp;        // 1 asignación
+                 
+                    String temp = palabras[j];
+                    palabras[j] = palabras[j + 1];
+                    palabras[j + 1] = temp;        
                 }
             }
         }
 
         System.out.println("\nDespués de ordenarlo quedo asi:");
 
-        for (String palabra : palabras) { // n operaciones
+        for (String palabra : palabras) {
             System.out.print(palabra + " ");
         }
     }
 }
-
-/*
-ANÁLISIS DE COMPLEJIDAD
-
-El algoritmo de ordenamiento burbuja utiliza dos ciclos anidados para recorrer el arreglo.
-
-El ciclo externo se ejecuta aproximadamente n veces.
-El ciclo interno también depende de n.
-
-Conteo aproximado de operaciones:
-
-T(n) = n(n - 1)
-
-T(n) = n² - n
-
-Notación asintótica:
-
-O(n²)
 
 Esto significa que el tiempo de ejecución del algoritmo crece proporcionalmente al cuadrado del tamaño del arreglo.
 */ 
